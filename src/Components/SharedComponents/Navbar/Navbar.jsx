65749613372c5f4bbf392e3c444242ebe975876e
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-11-27 17:20:10 +0600

    Navbar created for mobile and desktop and tablet view

--- src/Components/SharedComponents/Navbar/Navbar.jsx
+++ src/Components/SharedComponents/Navbar/Navbar.jsx
@@ -5,6 +5,9 @@ import { IoHomeOutline } from "react-icons/io5";
 import { TbCategory } from "react-icons/tb";
 import { GoPeople } from "react-icons/go";
 import { IoIosContact } from "react-icons/io";
+import { IoMdLogIn } from "react-icons/io";
+import { FaAngleDown } from "react-icons/fa";
+import { FaAngleUp } from "react-icons/fa";
 import { useState } from "react";
 
 const Navbar = () => {
@@ -32,66 +35,108 @@ const Navbar = () => {
           </Link>
         </div>
 
-
-
-
         {/* Link For mobile view */}
         {isOpen && (
-          <div className="flex flex-col gap-2 font-semibold text-xl absolute top-14 bg-gray-200 w-40 p-4">
-            <NavLink to="/" className="hover:text-green-500 flex items-center gap-1">
-            <IoHomeOutline /> Home
+          <div className="flex flex-col gap-2 font-semibold text-xl absolute top-14 bg-gray-200 w-60 p-4">
+            <NavLink
+              to="/"
+              className="hover:text-green-500 flex items-center gap-2"
+            >
+              <IoHomeOutline /> Home
             </NavLink>
             <div className="relative group">
-              <div onClick={() => setIsShow(!isShow)} className={isShow?'text-green-500 flex items-center gap-1':''} ><TbCategory /> All Category</div>
+              <div
+                onClick={() => setIsShow(!isShow)}
+                className={`flex items-center gap-2 ${
+                  isShow ? "text-green-500" : ""
+                }`}
+              >
+                <TbCategory /> All Category{" "}
+                {isShow ? (
+                  <FaAngleDown className="mt-1" />
+                ) : (
+                  <FaAngleUp className="mt-1" />
+                )}
+              </div>
               {isShow && (
                 <div className="flex flex-col gap-2 pl-4">
-                  <NavLink className="hover:text-green-500 flex items-center gap-1">Test1</NavLink>
-                  <NavLink className="hover:text-green-500 flex items-center gap-1">Test1</NavLink>
-                  <NavLink className="hover:text-green-500 flex items-center gap-1">Test1</NavLink>
+                  <NavLink className="hover:text-green-500">
+                    Pure village products
+                  </NavLink>
+                  <NavLink className="hover:text-green-500">Sweets</NavLink>
+                  <NavLink className="hover:text-green-500">Fruits</NavLink>
+                  <NavLink className="hover:text-green-500">Vegetable</NavLink>
+                  <NavLink className="hover:text-green-500">Ritual</NavLink>
+                  <NavLink className="hover:text-green-500">Store</NavLink>
                 </div>
               )}
             </div>
-            <NavLink to="/about" className="hover:text-green-500 flex items-center gap-1">
-            <GoPeople /> About Us
+            <NavLink
+              to="/about"
+              className="hover:text-green-500 flex items-center gap-2"
+            >
+              <GoPeople /> About Us
             </NavLink>
-            <NavLink to="/contact" className="hover:text-green-500 flex items-center gap-1">
-            <IoIosContact /> Contact Us
+            <NavLink
+              to="/contact"
+              className="hover:text-green-500 flex items-center gap-2"
+            >
+              <IoIosContact /> Contact Us
             </NavLink>
           </div>
         )}
       </div>
 
-
-
-
-
-
       {/* Link For Desktop and Tab View */}
       <div className="hidden md:flex flex-row gap-4 font-semibold text-xl">
-        <NavLink to="/" className="hover:text-green-500 flex items-center gap-1">
-        <IoHomeOutline /> Home
+        <NavLink
+          to="/"
+          className="hover:text-green-500 flex items-center gap-1"
+        >
+          <IoHomeOutline /> Home
         </NavLink>
         <div className="relative group">
-          <NavLink to="/category" className="hover:text-green-500 flex items-center gap-1">
-          <TbCategory /> All Category
+          <NavLink
+            to="/category"
+            className="hover:text-green-500 flex items-center gap-1"
+          >
+            <TbCategory /> All Category
           </NavLink>
-          <div className="absolute w-full p-2 rounded-lg hidden shadow-lg bg-white group-hover:block z-50">
+          <div className="absolute w-52 p-2 rounded-lg hidden shadow-lg bg-white group-hover:block z-50">
             <div className="flex flex-col">
-              <NavLink className="hover:text-green-500">Test1</NavLink>
-              <NavLink className="hover:text-green-500">Test1</NavLink>
-              <NavLink className="hover:text-green-500">Test1</NavLink>
+              <NavLink className="hover:text-green-500">
+                Pure village products
+              </NavLink>
+              <NavLink className="hover:text-green-500">Sweets</NavLink>
+              <NavLink className="hover:text-green-500">Fruits</NavLink>
+              <NavLink className="hover:text-green-500">Vegetable</NavLink>
+              <NavLink className="hover:text-green-500">Ritual</NavLink>
+              <NavLink className="hover:text-green-500">Store</NavLink>
             </div>
           </div>
         </div>
-        <NavLink to="/about" className="hover:text-green-500 flex items-center gap-1">
-        <GoPeople /> About Us
+        <NavLink
+          to="/about"
+          className="hover:text-green-500 flex items-center gap-1"
+        >
+          <GoPeople /> About Us
         </NavLink>
-        <NavLink to="/contact" className="hover:text-green-500 flex items-center gap-1">
-        <IoIosContact /> Contact Us
+        <NavLink
+          to="/contact"
+          className="hover:text-green-500 flex items-center gap-1"
+        >
+          <IoIosContact /> Contact Us
         </NavLink>
       </div>
       {/* For user or Login */}
-      <div>Login</div>
+      <div>
+        <Link
+          to="/login"
+          className="flex items-center gap-1 border-2 border-green-500 px-5 py-1 text-lg font-medium rounded-md hover:bg-green-500"
+        >
+          <IoMdLogIn /> Login
+        </Link>
+      </div>
     </div>
   );
 };
